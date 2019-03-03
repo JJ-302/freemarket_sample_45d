21bf26db46526933e8fd2baa5d4ed1a0bd23047e
beginerbeginer190105 <miyabi6125@gmail.com>
2019-03-03 20:19:44 +0900

    edit config/routes.rb
    コンフリクト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
 root 'products#index'
-<<<<<<< HEAD
-resources :products, only: [:new, :show]
-=======
 resources :products, only: [:new, :show] do
     get 'confirm_purchase', on: :member
     end
->>>>>>> JJ-302/master
+resources :users, only: :index
 end
